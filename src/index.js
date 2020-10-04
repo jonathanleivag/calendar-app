6dd82a8ed80a8bb6caaa29bd39b918640075ae88
Jonathanleivag <email@jonathanleivag.com>
2020-10-04 03:41:46 -0300

    App structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { CalendarApp } from './CalendarApp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalendarApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
